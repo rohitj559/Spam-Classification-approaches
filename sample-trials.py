f445885a8dc064becff38760963c8440a2adac45
Rohit Jagannath <rohitj559@gmail.com>
2018-12-06 14:15:48 -0600

    Update sample-trials.py

--- sample-trials.py
+++ sample-trials.py
@@ -5,19 +5,7 @@ Created on Wed Nov 28 22:10:21 2018
 @author: Rohit
 """
 
-# Sample Trials 
-
-# =============================================================================
-# fin = open('samp3.txt', "r")
-# data1 = fin.read()
-# fin.close()
-# fout = open('samp2.txt', "a")
-# fout.write(data1)
-# fout.close()
-# =============================================================================
-
 import os
-import string
 from nltk import word_tokenize
 import numpy as np
 import pandas as pd
@@ -34,7 +22,10 @@ h.seek(0,0)
 
 ham_path = 'C:\\Users\\Rohith\\Desktop\\Fall_2018\\Research\\Exercise8_Spam_Play_with_text\\github-code\\Spam-Classification-approaches\\text-converted-mails\\ham'
 spam_path = 'C:\\Users\\Rohith\\Desktop\\Fall_2018\\Research\\Exercise8_Spam_Play_with_text\\github-code\\Spam-Classification-approaches\\text-converted-mails\\spam'  
-    
+ 
+##################################################################################################
+# conversion of ham and spam from text to digits  
+##################################################################################################  
 ham_folder = os.listdir(ham_path)
 spam_folder = os.listdir(spam_path)
 #remove = dict.fromkeys(map(ord, '\n ' + string.punctuation.replace(' ','')))
@@ -43,6 +34,7 @@ digits_dict = {}
 digits_list = []
 count = 0
 
+# conversion of all ham mails into digits list
 ham_list = []
 for file in ham_folder:
     source = os.path.join(ham_path, file)
@@ -59,23 +51,14 @@ for file in ham_folder:
             fin_digits_list.append(digits_dict[word])
         else:
             fin_digits_list.append(digits_dict[word])
+# test
 # =============================================================================
 #     if len(fin_digits_list) < 15:
 #         print("Warning! on File: " + str(file));
 # =============================================================================
     digits_list.extend(fin_digits_list)
-
-# =============================================================================
-#     ham_list.append(fin.read())
-#     fin.close()
-# =============================================================================
-
-# =============================================================================
-# for item in digits_list:
-#     h.write("%s " % item)
-# h.close()
-# =============================================================================
     
+# conversion of spam mails into digits dictioanry    
 digits_dict2 = {}
 digits_list2 = []
 count2 = 0
@@ -99,12 +82,17 @@ for file in spam_folder:
     if len(fin_digits_list2) < 10:
         print("Warning! on File: " + str(file));
     digits_list2.extend(fin_digits_list2)
- 
-# size of ham numeric list(digits_list) and spam numeric list(digits_list2)
-print(len(digits_list));
-print(len(digits_list2));
 
-# if we need 32X32X32 matrices of ham and spam lists:
+# test 
+# size of ham numeric list(digits_list) and spam numeric list(digits_list2)
+# =============================================================================
+# print(len(digits_list));
+# print(len(digits_list2));
+# =============================================================================
+
+##################################################################################################
+# preprocessing to split the digits list obtained above into 32X32X3 chunk matrices
+##################################################################################################
 unitMatrixSize = 32;
 no_of_channels = 3;
 # unitSize = (no_of_channels*unitMatrixSize*unitMatrixSize);
@@ -146,13 +134,22 @@ for i in range(0, len(spam_digits_list), unitSize):
 # conversion of lists to arrays
 hamArray = np.array(hamMatrixList);
 spamArray = np.array(spamMatrixList); 
-     
+
+##################################################################################################
+# Extracting features using deep learning models
+##################################################################################################
 # Training the Resnet model    
 from keras.applications.resnet50 import ResNet50
 resnet_model = ResNet50(input_shape=(32, 32, 3), weights='imagenet', include_top=False)  # Since top layer is the fc layer used for predictions
-print("Total Params:", resnet_model.count_params()) # total no of parameters
 
+# test
+# =============================================================================
+# print("Total Params:", resnet_model.count_params()) # total no of parameters
+# =============================================================================
+
+##################################################################################################
 # extract the features for ham and spam seperately
+##################################################################################################
 ham_FeatureArray = resnet_model.predict(hamArray);
 spam_FeatureArray = resnet_model.predict(spamArray);
 
@@ -162,16 +159,26 @@ spamDF = pd.DataFrame(spam_FeatureArray.squeeze())
 hamDF["label"] = [0]*len(hamDF)
 spamDF["label"] = [1]*len(spamDF)
 
+# test
+# =============================================================================
+# hamDF["label"].head()
+# spamDF["label"].head()
+# =============================================================================
+
 frames = [hamDF, spamDF]
 df = pd.concat(frames)
 
 df = df.sample(frac=1).reset_index(drop=True)
 
+##################################################################################################
 # Splitting the dataset into  Training set and Test set
+##################################################################################################
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(df.drop(['label'], axis=1), df['label'], test_size=0.2, random_state=0)
 
-
+##################################################################################################
+# Classification models
+##################################################################################################
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
@@ -185,6 +192,13 @@ cm = confusion_matrix(y_test, y_pred)
 
 # Calculate accuracy
 accuracy = (cm[0][0] + cm[1][1]) / (cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])
+##################################################################################################
+
+##################################################################################################
+# classification models with grid search and cross validation
+##################################################################################################
+
+# Logistic Regression
 
 # Create regularization penalty space
 penalty = ['l1', 'l2']
@@ -213,100 +227,11 @@ print('Best C:', best_model.best_estimator_.get_params()['C'])
 # prediction on test data using the best model 
 y_pred = best_model.predict(X_test)
 
-# test
-# =============================================================================
-# hamDF["label"].head()
-# spamDF["label"].head()
-# =============================================================================
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.ensemble import IsolationForest
-from sklearn import svm
-
-
-
-
-
-
-# =============================================================================
-# # conversion of lists to arrays
-# hamArray = np.array(list_of_ndarrays_Ham);
-# spamArray = np.array(list_of_ndarrays_Spam);  
-# =============================================================================
-
-# =============================================================================
-# # generation of spam matrices
-# countSpam = 0
-# list_of_ndarrays_Spam = []
-# for i in range(spam_samples):
-#     list_size = countSpam + unitSize;
-#     temp_array =  np.reshape(digits_list2[countSpam:list_size],(unitMatrixSize, unitMatrixSize, no_of_channels));
-#     list_of_ndarrays_Spam.append(temp_array);
-#     countSpam = list_size;
-# =============================================================================
-
-
-# generation of ham matrices
-# =============================================================================
-# countHam = 0
-# list_of_ndarrays_Ham = []
-# for i in range(ham_samples):
-#     list_size = countHam + unitSize;
-#     temp_array =  np.reshape(digits_list[countHam:list_size],(unitMatrixSize, unitMatrixSize, no_of_channels));
-#     list_of_ndarrays_Ham.append(temp_array);
-#     count = list_size;
-# =============================================================================
-
-# =============================================================================
-# # sublist of 32*32 = 64 digits
-# hamSublist = digits_list[]
-# stepSize = 64;
-# for i in range(len(digits_list)):
-#     hamSublist = digits_list[i:stepSize];
-#     i+=
-#     
-# seq[0:high:unitSize]
-# 
-# seq = range(0, 100)
-# seqNew = seq[0:50:5] 
-# 
-# =============================================================================
-
-
-# =============================================================================
-# A = np.arange(27)
-# B = np.reshape(A,(3,3,3))
-# print(B[:,:,:])
-# =============================================================================
-        
-# =============================================================================
-# spam_list = []
-# for file in spam_folder:
-#     source = os.path.join(spam_path, file)
-#     fin = open(source, "r", encoding="utf-8")    
-#     fin.seek(0,0)
-#     spam_list.append(fin.read())
-#     fin.close()
-# 
-# for item in spam_list:
-#     s.write("%s\n" % item.lower().encode('utf8'))
-# s.close()
-# 
-# # Display punctuation.
-# for c in string.punctuation:
-#     print("[" + c + "]")
-# =============================================================================
-    
-
-
-
-
-
-
-        
-    
-
+# Calculate accuracy
+accuracy = (cm[0][0] + cm[1][1]) / (cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])
 
 
